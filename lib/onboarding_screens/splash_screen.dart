a1973dbd3b4ead2ebc42ee6aefe5f78b7452fba0
obehi234 <obehi.ataga234@gmail.com>
2024-02-07 22:32:00 +0100

    Splash screen created

--- lib/onboarding_screens/splash_screen.dart
+++ lib/onboarding_screens/splash_screen.dart
@@ -10,6 +10,11 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Image.asset('assets/images/styleLogo.png'),
+      ),
+    );
   }
 }
