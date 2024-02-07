855bacc31aa98d46b91c50e8371ef09defeedcfb
obehi234 <obehi.ataga234@gmail.com>
2024-02-07 23:25:26 +0100

    Remove decorator for dot indicator

---
+++ lib/onboarding_screens/slider.dart
@@ -0,0 +1,128 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:ecommerceapp/onboarding_screens/splash_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class IntroductorySliderPage extends StatefulWidget {
+  const IntroductorySliderPage({super.key});
+
+  @override
+  State<IntroductorySliderPage> createState() => _IntroductorySliderPageState();
+}
+
+class _IntroductorySliderPageState extends State<IntroductorySliderPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const SplashScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(32.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.symmetric(vertical: 20),
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      allowImplicitScrolling: true,
+      autoScrollDuration: 3000,
+      infiniteAutoScroll: true,
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.only(top: 16, right: 16),
+          child: TextButton(
+            child: const Text('Skip',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+            onPressed: () => _onIntroEnd(context),
+          ),
+        )),
+      ),
+
+      pages: [
+        PageViewModel(
+          title: "Fractional shares",
+          body:
+              "Instead of having to buy an entire share, invest any amount you want.",
+          image: Image.asset('assets/images/slider1.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Learn as you go",
+          body:
+              "Download the Stockpile app and master the market with our mini-lesson.",
+          image: Image.asset('assets/images/slider2.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Kids and teens",
+          body:
+              "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+          image: Image.asset('assets/images/slider3.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Title of last page - reversed",
+          bodyWidget: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Click on ", style: bodyStyle),
+              Icon(Icons.edit),
+              Text(" to edit a post", style: bodyStyle),
+            ],
+          ),
+          decoration: pageDecoration.copyWith(
+            bodyFlex: 2,
+            imageFlex: 4,
+            bodyAlignment: Alignment.bottomCenter,
+            imageAlignment: Alignment.center,
+          ),
+          image: Image.asset('assets/images/slider3.png'),
+          reverse: true,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: false,
+
+      back: const Icon(Icons.arrow_back),
+      skip: const Text('Prev', style: TextStyle(fontWeight: FontWeight.w600)),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Next', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      // dotsContainerDecorator: const ShapeDecoration(
+      //   color: Colors.white,
+      //   shape: RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      //   ),
+      // ),
+    );
+  }
+}
--- lib/style_app.dart
+++ lib/style_app.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerceapp/onboarding_screens/slider.dart';
 import 'package:ecommerceapp/onboarding_screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,13 +8,8 @@ class StyleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
+     
+      home: const IntroductorySliderPage(),
     );
   }
 }
