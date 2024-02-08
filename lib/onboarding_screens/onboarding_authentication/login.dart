3dad77554674b6cd7ac5107b2013b39a9f7ba447
obehi234 <obehi.ataga234@gmail.com>
2024-02-08 10:10:36 +0100

    Trying to set up textfield

--- lib/onboarding_screens/onboarding_authentication/login.dart
+++ lib/onboarding_screens/onboarding_authentication/login.dart
@@ -8,28 +8,54 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 28.0),
-          child: ListView(children: const [
-            SizedBox(
-              height: 50,
+    return Scaffold(
+      body: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 28.0),
+            child: ListView(
+              children: const [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Welcome\nBack!',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Colors.black,
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    
+                    decoration: InputDecoration(
+                      filled: true,
+                      hintText: 'Username or Email',
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.black, width: 2),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Welcome\nBack!',
-              style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  color: Colors.black,
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.none),
-            ),
-            
-
-          ],),
-        ));
+          )),
+    );
   }
 }
