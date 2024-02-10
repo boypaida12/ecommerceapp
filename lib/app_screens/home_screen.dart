ffd537b7a8ce296e9b14b652ec8f34b60faa2ba4
obehi234 <obehi.ataga234@gmail.com>
2024-02-10 10:57:17 +0100

    Creating pill widget for my UI

--- lib/app_screens/home_screen.dart
+++ lib/app_screens/home_screen.dart
@@ -11,17 +11,69 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Row(children: [
-        Image.asset('assets/images/styleLogo.png',
-            fit: BoxFit.contain, height: 32),
-        Text('Stylish')
-      ]),
-      actions: [
-        CircleAvatar(
-          backgroundImage: AssetImage('assets/images/avatar.jpg'),
-        )
-      ],
-    ));
+      appBar: AppBar(
+        backgroundColor: Colors.grey[200],
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: Container(
+          constraints: BoxConstraints(maxWidth: 200),
+          child: Center(
+            child: Row(children: [
+              Image.asset('assets/images/styleLogo.png',
+                  fit: BoxFit.contain, height: 32),
+              SizedBox(width: 5),
+              Text('Stylish')
+            ]),
+          ),
+        ),
+        actions: const [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/avatar.jpg'),
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: ListView(children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: Icon(Icons.mic),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Search any Product ',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:[
+            Text('All Featured', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            Row(children: [
+              
+
+            ],)
+          ],)
+        ]),
+      ),
+    );
+  }
+}
+
+class PillWidget extends StatelessWidget {
+  const PillWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
