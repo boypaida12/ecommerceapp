e38c6247f0b4cbe78af7e4660154d8c62bb0e410
boypaida12 <baddysackey5@gmail.com>
2024-02-14 08:03:15 +0000

    Update custom bottom sheet with comments on how the widget was created and how it should be implemented

--- lib/shared/custombottomsheet.dart
+++ lib/shared/custombottomsheet.dart
@@ -3,27 +3,37 @@ import 'package:flutter/material.dart';
 
 // copy the code below to show the bottomsheet via the custom button in the screen you are working
 
+// start copying from here
     // CustomButton(buttonText: 'Show Bottom Sheet', onPressed: (){
-    // showModalBottomSheet(context: context, builder: (context) => CustomBottomSheet());
+    // showModalBottomSheet(context: context, builder: (context) => CustomBottomSheet(
+    //  child: Column() //can change Column to any widget depending on your screen's use case
+    // ));
     // }),
+// end copying here
 
+
+// This widget creates a bottom sheet that can contain any other widget as its content.
 class CustomBottomSheet extends StatelessWidget {
+    // The widget that will be displayed inside the bottom sheet.
+    // final means the child cannot be changed once the box.
+    // Widget refers to any element you can build in Flutter. In our case it will likely be a Column widget. 
   final Widget child;
 
   const CustomBottomSheet({
     Key? key,
-    required this.child,
+    required this.child, // making the widget required means it always has to be called before the widget can work.
     }) : super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
+    // Use the BottomSheet widget to create the bottom sheet.
     return BottomSheet(
       onClosing: () {
-        Navigator.pop(context);
+        Navigator.pop(context); // When the user taps outside the sheet, close it.
       },
       builder: (context) => Container(
-        child: child,
+        child: child, // the child Widget that is required.
       ),
     );
   }
